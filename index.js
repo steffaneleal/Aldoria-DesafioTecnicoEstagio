f07c82f71df2be125a04e4e86d7af4a7454e111f
Stéffane <steffane.leal13@gmail.com>
2024-02-14 23:57:29 -0300

    versão 9
    carrossel feito

--- index.js
+++ index.js
@@ -15,3 +15,36 @@ const btnAnimation = document.getElementById('header-nav-bt');
 
 menu.addEventListener('click', animation);
 
+
+/*carousel*/
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+const gallery = document.querySelector(".gallery");
+
+controls.forEach((control) => {
+  control.addEventListener("click", () => {
+    const isLeft = control.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    const itemWidth = items[currentItem].offsetWidth + 15; 
+    gallery.style.transform = `translateX(${-currentItem * itemWidth}px)`;
+
+    items.forEach((item) => item.classList.remove("current-item"));
+    items[currentItem].classList.add("current-item");
+  });
+});
